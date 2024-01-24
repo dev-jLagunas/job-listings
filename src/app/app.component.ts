c913907e927ea2a46baf0c4eb4e167b28b70d88e
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-24 12:53:12 +0900

    Populate html with data and create content for styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'job-listing';
+export class AppComponent implements OnInit {
+  jobData: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('/assets/data.json').subscribe((data) => {
+      this.jobData = data;
+    });
+  }
 }
