bbd5b66fb46a990fea8e645b5f8efcb1eaef0203
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-24 19:01:51 +0900

    Complete typescript filtering logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,24 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { NgFor } from '@angular/common';
 
+// Structure of job objest in data.json
+interface Job {
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+  bookmarked: boolean;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,13 +30,47 @@ import { NgFor } from '@angular/common';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  jobData: any;
+  jobData: Job[] = [];
+  filters: string[] = [];
+  filteredJobs: Job[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('/assets/data.json').subscribe((data) => {
+    this.http.get<Job[]>('/assets/data.json').subscribe((data: Job[]) => {
       this.jobData = data;
+      this.filteredJobs = [...this.jobData];
     });
+  }
+
+  toggleBookmark(job: any) {
+    job.bookmarked = !job.bookmarked;
+  }
+
+  addFilter(filter: string) {
+    this.filters.push(filter);
+    this.filterJobs();
+  }
+
+  removeFilter(filter: string) {
+    this.filters = this.filters.filter((f) => f !== filter);
+    this.filterJobs();
+  }
+
+  filterJobs() {
+    this.filteredJobs = this.jobData.filter((job: Job) =>
+      this.filters.every(
+        (filter) =>
+          job.role === filter ||
+          job.level === filter ||
+          job.languages.includes(filter) ||
+          job.tools.includes(filter)
+      )
+    );
+  }
+
+  clearFilters() {
+    this.filters = [];
+    this.filteredJobs = [...this.jobData];
   }
 }
