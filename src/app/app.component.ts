a3e784ac366eff4245a6e7dcf36d40a265651bc3
Juan Lagunas <dev.jlagunas@gmail.com>
2024-03-17 12:57:05 +0900

    Refactor: Update AppComponnt to use observables and async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,48 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { NgFor } from '@angular/common';
-
-// Structure of job objest in data.json
-interface Job {
-  id: number;
-  company: string;
-  logo: string;
-  new: boolean;
-  featured: boolean;
-  position: string;
-  role: string;
-  level: string;
-  postedAt: string;
-  contract: string;
-  location: string;
-  languages: string[];
-  tools: string[];
-  bookmarked: boolean;
-}
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs';
+import { Job } from './job.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgFor],
+  imports: [CommonModule, RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  jobData: Job[] = [];
+  jobData$: Observable<Job[]> | undefined;
   filters: string[] = [];
-  filteredJobs: Job[] = [];
+  filteredJobs$: Observable<Job[]> | undefined;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<Job[]>('/assets/data.json').subscribe((data: Job[]) => {
-      this.jobData = data;
-      this.filteredJobs = [...this.jobData];
-    });
+    this.jobData$ = this.http.get<Job[]>('/assets/data.json').pipe(
+      catchError((error) => {
+        console.error('Error fetching job data:', error);
+        return throwError(
+          () => new Error('Error fetching job data. Please try again later.')
+        );
+      })
+    );
+
+    this.filterJobs();
   }
 
-  toggleBookmark(job: any) {
+  toggleBookmark(job: Job) {
     job.bookmarked = !job.bookmarked;
   }
 
@@ -58,19 +49,32 @@ export class AppComponent implements OnInit {
   }
 
   filterJobs() {
-    this.filteredJobs = this.jobData.filter((job: Job) =>
-      this.filters.every(
-        (filter) =>
-          job.role === filter ||
-          job.level === filter ||
-          job.languages.includes(filter) ||
-          job.tools.includes(filter)
-      )
-    );
+    if (this.jobData$) {
+      this.filteredJobs$ = this.jobData$.pipe(
+        map((jobs: Job[]) => {
+          return jobs.filter((job: Job) =>
+            this.filters.every(
+              (filter) =>
+                job.role === filter ||
+                job.level === filter ||
+                job.languages.includes(filter) ||
+                job.tools.includes(filter)
+            )
+          );
+        })
+      );
+    }
   }
 
   clearFilters() {
     this.filters = [];
-    this.filteredJobs = [...this.jobData];
+    if (this.jobData$) {
+      this.jobData$.subscribe((data: Job[]) => {
+        this.filteredJobs$ = new Observable((subscriber) => {
+          subscriber.next(data);
+          subscriber.complete();
+        });
+      });
+    }
   }
 }
---
+++ src/app/job.model.ts
@@ -0,0 +1,17 @@
+// Structure of job object in data.json
+export interface Job {
+  id: number;
+  company: string;
+  logo: string;
+  new: boolean;
+  featured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+  bookmarked: boolean;
+}
